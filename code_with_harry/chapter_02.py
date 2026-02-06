d2f8949fe031fcb98aaef98f8234f66f5cb2e1cc
ananya-codes-data <ananyapradhan7543@gmail.com>
2026-02-07 01:02:31 +0530

    add: chapter 2

---
+++ code_with_harry/chapter_02.py
@@ -0,0 +1,140 @@
+# variables
+
+# a = 1
+# b = 2
+# c = 7
+# name = "Ananya"
+
+# print(name)
+
+# variables = container to store a value.
+# keywords = reserved words in python 
+# identifiers = class/function/variable name
+
+
+# data types
+
+# a = 22 # a is an integer
+# b = 21.22 # b is a floating point number
+# c = "ananya" # c is a string
+# d = False # d is a boolean variable
+# e = None # e is a none type variable
+
+
+# rules of choosing variables
+
+# a = 22
+# aaa = 1234
+# ananya = 2134
+# shan = 2608
+# _shan = 2010
+# ananya_2112 = ananya
+
+# @sameer = 56 # Invalid due to @ symbol
+# s@meer # Invalid due to @ symbol
+
+
+# operators in python
+
+# 1. Arithmetic operators: +, -, *, / etc.
+# 2. Assignment operators: =, +=, -= etc.
+# 3. Comparison operators: ==, >, >=, <, != etc.
+# 4. Logical operators: and, or, not.
+
+# Arithmetic Operators
+
+# 7 + 4 = 11
+# 7 & 4 are operands
+# + is the operator
+# 11 is the result
+
+# a = 21
+# b = 22
+# c = a + b
+
+# print(c)
+
+# Assignment operators
+
+# a = 4 - 2 # Assign 4 - 2 in a
+# print(a)
+# b = 6
+
+# b += 3 # Increment the value of b by 3 and then assign it to b
+# b -= 3 # Decrement the value of b by 3 and then assign it to b
+# b *= 3 # Multiply the value of b with 3 and then assign it to b
+# b /= 3 # Divide the value of b by 3 and then assign it to b
+
+# print(b)
+
+# Comparison operators
+
+# d = 5 < 4 # Is 5 less than 4
+# d = 5 > 4 # Is 5 more than 4
+# d = 5 >= 5 # Is 5 more than equal to 5
+# d = 5 <= 5 # Is 5 less than equal to 5
+# d = 5 != 7 # Is 5 not equal to 7
+# d = 5 == 5 # Is 5 equal to 5
+
+# print(d)
+
+
+# Logical operators
+
+# e = True or False
+
+# Truth table of 'or'
+# print("True or False is ", True or False)
+# print("True or True is ", True or True)
+# print("False or True is ", False or True)
+# print("False or False is ", False or False)
+
+# Truth table of 'and'
+# print("True and False is ", True and False)
+# print("True and True is ", True and True)
+# print("False and True is ", False and True)
+# print("False and False is ", False and False)
+
+# multi cursor functionality
+# press alt and where ever you click cursor will apply and we can do that multiple times
+
+
+# print(not(False))
+
+# print(not(True))
+
+
+# type function - function to find data type of any variable
+
+# a = 31
+# t = type(a) # class <int>
+
+# a = 31.2
+# t = type(a) # class <float>
+
+# a = "ananya"
+# t = type(a) # class <str>
+
+# a = "22.10"
+# t = type(a) # class <str>
+
+# a = "22.10"
+# b = float(a) # a but the type should be float
+# t = type(b) # class <float>
+
+# print(t)
+
+# str(31) =>"31" # integer to string conversion
+# int("32") => 32 # string to integer conversion
+# float(32) => 32.0 # integer to float conversion
+
+
+# input function -
+
+a = input("Enter number 1: ")
+b = input("Enter number 2: ")
+
+print("Number a is: ", a)
+print("Number b is: ", b)
+
+# alt + shift + down arrow key - to replicate a line
