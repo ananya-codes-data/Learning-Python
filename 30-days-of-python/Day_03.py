e6a1348ce983d69af4480c014279bba906a64c36
ananya-codes-data <ananyapradhan7543@gmail.com>
2026-02-20 01:30:52 +0530

    add: day 3

---
+++ 30-days-of-python/Day_03.py
@@ -0,0 +1,79 @@
+# Operators
+
+# Note
+# 1. Declaring the variable at the top first
+# 2. then can go for applying functions to the assigned variable
+
+# example
+# Calculating area of a circle
+# radius = 10                                 # radius of a circle
+# area_of_circle = 3.14 * radius ** 2         # two * sign means exponent or power
+# print('Area of a circle:', area_of_circle)
+
+# Calculating area of a rectangle
+# length = 10
+# width = 20
+# area_of_rectangle = length * width
+# print('Area of rectangle:', area_of_rectangle)
+
+# Calculating a weight of an object
+# mass = 75
+# gravity = 9.81
+# weight = mass * gravity
+# print(weight, 'N')                         # Adding unit to the weight
+
+# Calculate the density of a liquid
+# mass = 75 # in Kg
+# volume = 0.075 # in cubic meter
+# density = mass / volume # 1000 Kg/m^3
+# print(density, 'Kg/m^3') # Adding unit to the density
+
+
+# is: Returns true if both variables are the same object(x is y)
+# is not: Returns true if both variables are not the same object(x is not y)
+# in: Returns True if the queried list contains a certain item(x in y)
+# not in: Returns True if the queried list doesn't have a certain item(x not in y)
+
+
+# Exercises - Day 3
+
+# 1.
+# age = 22
+
+# 2.
+# h = 5.8
+
+# 3.
+# c = 22 + 1j
+
+# 4.
+# base = float(input("Enter base:"))
+# height = float(input("Enter height:"))
+# area_of_traingle = 0.5 * base * height
+
+# print("The area of the triangle is", area)
+
+# 5.
+# side_a = float(input("Enter side a:"))
+# side_b = float(input("Enter side b:"))
+# side_c = float(input("Enter side c:"))
+# perimeter_of_triangle = side_a + side_b + side_c
+
+# print("The perimeter of the triangle is", perimeter)
+
+# 6.
+# length = float(input("Enter length:"))
+# width = float(input("Enter width:"))
+# area_of_rectangle = length * width
+# perimeter_of_rectangle = 2 * (length + width)
+
+# print("The area of the rectangle is", area, "and", "The perimeter of the rectangle is", perimeter)
+
+# 7.
+# radius = float(input("Enter radius:"))
+# pi = 3.14
+# area_of_circle = pi * radius * radius
+# circumference_of_circle = 2 * pi * radius
+
+# print("The area of the circle is", area_of_circle)
+# print("The circumference of the circle is", circumference_of_circle)
