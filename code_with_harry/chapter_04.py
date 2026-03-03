84389a24db78a54eb5bef849123937f4631a7fd7
ananya-codes-data <ananyapradhan7543@gmail.com>
2026-03-03 23:53:38 +0530

    add: started with day 4

--- code_with_harry/chapter_04.py
+++ code_with_harry/chapter_04.py
@@ -1,1 +1,15 @@
 # Lists and Tuples
+
+# List
+# containers to store a set of values of any data type
+# unlike strings, lists are mutable i.e., it can be changed
+
+friends = ["Apple", "Orange", 5, 345.06, False, "Aakash", "Rohan"]
+print(friends[0])
+
+friends[0] = "Grapes"
+
+print(friends[0])
+
+# lists can be indexed like string
+print(friends[1:4])
