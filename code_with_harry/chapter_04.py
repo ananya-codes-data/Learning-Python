cb69e0d238e9d97a3353b9936db0e13d91ddeb4c
ananya-codes-data <ananyapradhan7543@gmail.com>
2026-03-04 23:57:33 +0530

    add: day 4- list methods

--- code_with_harry/chapter_04.py
+++ code_with_harry/chapter_04.py
@@ -5,11 +5,34 @@
 # unlike strings, lists are mutable i.e., it can be changed
 
 friends = ["Apple", "Orange", 5, 345.06, False, "Aakash", "Rohan"]
-print(friends[0])
+# print(friends[0])
 
-friends[0] = "Grapes"
+# friends[0] = "Grapes"
 
-print(friends[0])
+# print(friends[0])
 
 # lists can be indexed like string
-print(friends[1:4])
+# print(friends[1:4])
+
+
+# list methods
+
+# in string when we use any method it would result in a new string, we have to print it
+# but in list, the list itself gets changed and we don't have to print it
+
+# print(friends)
+
+# friends.append("Ananya")        # add any data at the end of the list
+
+# print(friends)
+
+l1 = [1, 34, 62, 2, 6, 11]
+
+# l1.sort()       # sorting of numerical list in increasing order
+# print(l1)
+
+# l1.reverse()
+# print(l1)       # reversing the complete list
+
+l1.insert(3, 2210)
+print(l1)
