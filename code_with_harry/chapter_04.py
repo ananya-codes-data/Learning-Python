c9f43182d1bba5b88a2c030703dc26c4b663fd89
ananya-codes-data <ananyapradhan7543@gmail.com>
2026-03-06 23:43:06 +0530

    add: chapter-4

--- code_with_harry/chapter_04.py
+++ code_with_harry/chapter_04.py
@@ -4,7 +4,7 @@
 # containers to store a set of values of any data type
 # unlike strings, lists are mutable i.e., it can be changed
 
-friends = ["Apple", "Orange", 5, 345.06, False, "Aakash", "Rohan"]
+# friends = ["Apple", "Orange", 5, 345.06, False, "Aakash", "Rohan"]
 # print(friends[0])
 
 # friends[0] = "Grapes"
@@ -34,5 +34,12 @@ l1 = [1, 34, 62, 2, 6, 11]
 # l1.reverse()      # reversing the complete list
 # print(l1)
 
-l1.insert(3, 2210)      # inserting object at particular index
+# l1.insert(3, 2210)      # inserting object at particular index
+# print(l1)               # insert 2210 such that its index in the list is 3
+
+# print(l1.pop(3))      # prints the value at the index
+# l1.pop(3)       # deletes element at the specific index
+# print(l1)       # returns the new list as result
+
+l1.remove(34)
 print(l1)
