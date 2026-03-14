21477e912a8154c40b4449238e633c37750237c1
ananya-codes-data <ananyapradhan7543@gmail.com>
2026-03-15 01:22:18 +0530

    add: day 5

---
+++ 30-days-of-python/Day_05.py
@@ -0,0 +1,36 @@
+# Lists
+
+# four collection data types in Python :
+
+# 1. List
+# a collection which is ordered and changeable(modifiable)
+# Allows duplicate members
+
+# 2. Tuple
+# collection which is ordered and unchangeable or unmodifiable(immutable)
+# Allows duplicate members
+
+# 3. Set
+# collection which is unordered, un-indexed and unmodifiable, but we can add new items to the set
+# Duplicate members are not allowed
+
+# 4. Dictionary
+# ollection which is unordered, changeable(modifiable) and indexed
+# No duplicate members
+
+
+# How to Create a List
+
+# Using list built-in function
+# a = list("Ananya")
+
+# Using square brackets, []
+b = [21, 12, True]
+
+# Lists can have items of different data types
+
+# Accessing List Items Using Positive Indexing
+# list index always starts from zero
+
+# print(b[0])
+
