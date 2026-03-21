a8387f49419f8f44b8975c888e6206ff2582a0ff
ananya-codes-data <ananyapradhan7543@gmail.com>
2026-03-22 02:21:59 +0530

    add: chapter 5

--- code_with_harry/chapter_05.py
+++ code_with_harry/chapter_05.py
@@ -2,6 +2,8 @@
 
 # Dictionary is a collection of keys-value pairs
 
+# d = {}      # empty dictionary
+
 # marks = {
 #     "Harry": 100,
 #     "Shubham": 56,
@@ -15,4 +17,40 @@
 # 2. It is mutable
 # 3. It is indexed
 # 4. Cannot contain duplicate keys
+
+# Dictionary methods
+# marks = {
+#     "Harry": 100,
+#     "Shubham": 56,
+#     "Rohan": 23,
+#     0: "Harry"
+# }
+
+# print(marks.items())    # Returns a list of (key,value) in form of tuples
+# print(marks.keys())     # Returns a list containing dictionary's keys
+# print(marks.values())       # Returns a list containing dictionary's vales
+# marks.update({"Harry": 99, "Renuka": 100})
+# a new dict is passed,
+# the values that are present beforehand are updated
+# and new key-value pairs are added
+# print(marks)
+
+# print(marks.get("Shivika"))     # None
+# print(marks.get("Harry"))       # 100
+
+# Interview question
+# diff between
+# print(marks["Harry"])
+# vs
+# print(marks.get("Harry"))
+# in both of the above cases the output will be same
+# but if there is a key which does not exist
+# print(marks["Harry2"]) -- will give an error
+# while
+# print(marks.get("Harry")) -- will print None as the output
+# TODO: SEARCH FOR MORE PYTHON DICTIONARY METHODS
+# pop, popitem
+
+# Sets
+# Set is a collection of non-repetitive elements
 
