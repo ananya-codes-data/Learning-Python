a63285fbe17815022e25b1f266b6cb24e3ff1986
ananya-codes-data <ananyapradhan7543@gmail.com>
2026-03-21 23:57:02 +0530

    add: chapter 5

--- code_with_harry/chapter_05.py
+++ code_with_harry/chapter_05.py
@@ -1,8 +1,18 @@
 # Dictionary and Sets
 
-marks = {
-    "Harry": 100,
-    "Shubham": 56,
-    "Rohan": 23
-}
-print(marks, type(marks))
+# Dictionary is a collection of keys-value pairs
+
+# marks = {
+#     "Harry": 100,
+#     "Shubham": 56,
+#     "Rohan": 23
+# }
+# print(marks, type(marks))
+# print(marks["Harry"])   # 100
+
+# properties of python dictionaries
+# 1. It is unordered
+# 2. It is mutable
+# 3. It is indexed
+# 4. Cannot contain duplicate keys
+
