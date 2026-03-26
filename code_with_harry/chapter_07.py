0da9d280e048b7bbcbdec3cc03e066ec9e657291
ananya-codes-data <ananyapradhan7543@gmail.com>
2026-03-26 23:52:44 +0530

    add: chapter 7

---
+++ code_with_harry/chapter_07.py
@@ -0,0 +1,70 @@
+# Loops in Python
+
+# for i in range(1, 6):
+#     print(i)
+
+# types of loops in python
+# while loops
+# for loops
+
+# while loop
+
+# i = 1
+
+# while(i < 6):
+#     print(i)
+#     i += 1
+
+# i = 1
+
+# while(i < 51):
+#     print(i)
+#     i += 1
+
+# i = 0
+
+# while(i < 5):
+#     print("Harry")
+#     i += 1
+
+# list using while loop
+
+# l = [1, 10.22, "Harry", False, "Rohan", "This", "Shubham"]
+
+# i = 0
+
+# while(i < len(l)):
+#     print(l[i])
+#     i += 1
+
+# for loops
+
+# for loop is used to iterate through a sequence like list, tuple, or string [iterables]
+
+# for i in range(0, 4):
+#     print(i)
+
+# range(start, stop, step_size)
+
+# for i in range(20,42):
+#     print(i)        # from 20 to 41 and excluding 42
+
+# for i in range(20, 62, 4):
+#     print(i)        # 20, 24, 28, 32,...,56, 60
+
+# for loop iterate
+
+# l = [1, 4, 6, 224, 21, 6, 764]
+
+# for i in l:
+#     print(i)
+
+# t = (6, 231, 75, 122)
+
+# for i in t:
+#     print(i)
+
+# s = "Ananya"
+
+# for i in s:
+#     print(i)
