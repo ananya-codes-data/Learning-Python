45466de17b436bf3c7412283f255a3cf67431af0
ananya-codes-data <ananyapradhan7543@gmail.com>
2026-03-27 23:46:06 +0530

    add: day 7

--- code_with_harry/chapter_07.py
+++ code_with_harry/chapter_07.py
@@ -54,17 +54,91 @@
 
 # for loop iterate
 
+# for loop with lists
+
 # l = [1, 4, 6, 224, 21, 6, 764]
 
 # for i in l:
 #     print(i)
+
+# for loop with tuples
 
 # t = (6, 231, 75, 122)
 
 # for i in t:
 #     print(i)
 
+# for loop with string
+
 # s = "Ananya"
 
 # for i in s:
 #     print(i)
+
+# for loop with else
+
+# l = [1, 7, 8]
+
+# for item in l:
+#     print(item)
+
+# else:
+#     print("done")  # this is printed when the loop exhausts!
+
+
+# break statement
+
+# for i in range(0, 100):
+#     if(i == 34):
+#         break   # exit the loop right now
+#     print(i)
+
+# continue statement
+
+# for i in range(0, 100):
+#     if(i == 34):
+#         continue   # skip this iteration
+#     print(i)
+
+# pass statement - null statement in python
+
+# for i in range(0, 645):
+#     pass
+
+# i = 0
+
+# while(i < 45):
+#     print(i)
+#     i += 1
+
+
+# Practice Set
+
+# 1.
+
+# n = int(input("Enter a number:"))
+
+# for i in range(1, 11):
+#     print(f"{n} X {i} = {n * i}")
+
+# 2.
+
+# l = ["Harry", "Soham", "Sachin", "Rahul"]
+
+# for name in l:
+#     if(name.startswith("S")):
+#         print(f"Hello {name}")
+
+# 3.
+
+# n = int(input("Enter a number:"))
+
+# i = 1
+
+# while(i < 11):
+#     print(f"{n} X {i} = {n * i}")
+#     i += 1
+
+# 4.
+
+n = int(input("Enter a number:"))
