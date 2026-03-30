6eb2dea9e6fda98332fe2fbaaf486cafdbd1b778
ananya-codes-data <ananyapradhan7543@gmail.com>
2026-03-30 23:46:15 +0530

    add: chapter 7

--- code_with_harry/chapter_07.py
+++ code_with_harry/chapter_07.py
@@ -153,9 +153,25 @@
 
 # 4.
 
-n = int(input("Enter a number:"))
+# n = int(input("Enter a number:"))
 
-for i in range(2, n):
-    if(n % i)  == 0:
-        print("Number is not prime")
-        break
+# for i in range(2, n):
+#     if(n % i)  == 0:
+#         print("Number is not prime")
+#         break
+# else:
+#     print("Number is prime")
+
+# 5.
+
+n = int(input("Enter the number:"))
+
+i = 1
+
+sum = 0
+
+while(i <= n):
+    sum += i
+    i += 1
+
+print(sum)
