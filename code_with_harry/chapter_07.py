7b276dc85bf0942e6346e524c6ab7ea86b3c8e12
ananya-codes-data <ananyapradhan7543@gmail.com>
2026-03-31 23:17:14 +0530

    add: chapter 7

--- code_with_harry/chapter_07.py
+++ code_with_harry/chapter_07.py
@@ -164,14 +164,35 @@
 
 # 5.
 
-n = int(input("Enter the number:"))
+# n = int(input("Enter the number:"))
 
-i = 1
+# i = 1
 
-sum = 0
+# sum = 0
 
-while(i <= n):
-    sum += i
-    i += 1
+# while(i <= n):
+#     sum += i
+#     i += 1
 
-print(sum)
+# print(sum)
+
+# 6.
+
+# 5! = 1 x 2 x 3 x 4 x 5
+
+# n = int(input("Enter the number:"))
+
+# product = 1
+
+# for i in range(1, n+1):
+#     product = product * i
+
+# print(f"The factorial of {n} is {product}")
+
+# 7.
+
+#   *
+#  ***
+# *****
+
+# for n = 3
